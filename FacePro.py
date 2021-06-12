54e939aa220f03a34e776122314360b079ed4e0b
llllTnTlll <lzy991111@icloud.com>
2021-06-12 11:54:21 +0800

    FacePro v1.0

--- FacePro.py
+++ FacePro.py
@@ -3,6 +3,7 @@ import train_model
 import face_recognition
 import face_load
 import cfg_manager
+import pickle_helper
 import sys
 
 
@@ -10,10 +11,11 @@ def show_menu():
     # 展示菜单
     print('\033[1;32m=======================Function_Menu========================\033[0m')
     print('press 1 for : FaceRecognition(SVM)')
-    print('press 2 for : LoadNewFace')
-    print('press 3 for : FaceEncoding & TrainModel')
-    print('press 4 for : Settings')
-    print('press 5 for : Exit')
+    print('press 2 for : FaceEncoding & TrainModel')
+    print('press 3 for : LoadNewFace')
+    print('press 4 for : UserManagement')
+    print('press 5 for : Settings')
+    print('press 6 for : Exit')
     key = input('\033[4;33menter num and press enter : \033[0m')
     function_choose(key)
 
@@ -22,10 +24,11 @@ def function_choose(num):
     # 功能选择
     numbers = {
         '1': do_recognition,
-        '2': do_load,
-        '3': do_train,
-        '4': do_settings,
-        '5': do_exit,
+        '2': do_train,
+        '3': do_load,
+        '4': do_facemanage,
+        '5': do_settings,
+        '6': do_exit,
     }
 
     numbers.get(num, default)()
@@ -71,6 +74,25 @@ def do_settings():
     elif key == '3':
         main()
     do_settings()
+
+
+def do_facemanage():
+    data = pickle_helper.load_pickle_from_disk(r'C:\Users\ZHIYUAN\PycharmProjects\Facepro\data\pickleHere\embeddings.pickle')
+    names = data["names"]
+    name_list = []
+    for name in names:
+        if name not in name_list:
+            name_list.append(name)
+    print('|  id  |      name      |')
+    for i in range(len(name_list)):
+        print(' %i       %s' % (i, name_list[i]))
+    flag = True
+    while flag:
+        try:
+            id = int(input('\033[4;33menter an id :\033[0m'))
+            flag = False
+        except ValueError:
+            print('no such id please enter again ')
 
 
 def do_exit():
--- README.md
+++ README.md
@@ -37,10 +37,10 @@ righteye_classifier_path = C:\Users\ZHIYUAN\PycharmProjects\Facepro\eye_detectio
 
 此文件夹用于存放照片，如要手动添加全新人脸，请在facedirectory中新建文件夹，并以**人名（最好是英文或是拼音）命名此文件夹**，此文件夹下的所有.jpg照片将被作为该用户画像的一部分录入程序，画像的数目越多，编码速度越慢，识别越准确，请谨慎添加，避免无效数据污染画像。
 ### 配置用来存放各部分运行结果的data文件夹
-data文件夹用于在本地保存编码和标签文件以及已拟合的分类器模型对应的pickle文件，请和上一步一样，在**本地创建空的data文件夹**，在第一次运行时，程序会自动创建pickle
+data文件夹用于在本地保存编码和标签文件以及已拟合的分类器模型对应的pickle文件，请和上一步一样，在**本地创建空的data文件夹，在data文件中创建pickleHere文件夹**，在第一次运行时，程序会自动创建pickle
 ### 使用支持向量机
 因为项目使用了SVM这种二分类算法，**请在第一次运行前，确保pickle文件中具有两个或两个以上的人脸编码文件，或是facedirectory中存在两个以上包含图片的人脸文件夹。** 这样程序才能正常构建lable标签文件，否则会产生 'classes must be greater than one' 错误。
-## 1.3 以上
+## 1.3 使用说明
 菜鸡作品请多包容
 
  **Have Fun**
