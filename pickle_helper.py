0e11b683378f89d74c18f8ccffd683db656e7f39
llllTnTlll <lzy991111@icloud.com>
2021-06-16 17:32:55 +0800

    FacePro v1.0

--- pickle_helper.py
+++ pickle_helper.py
@@ -1,4 +1,5 @@
 import pickle
+import face_encoding
 
 
 def write_pickle_to_disk(save_path, data):
@@ -13,3 +14,23 @@ def load_pickle_from_disk(load_path):
     file = open(r"%s" % load_path, "rb")
     f = pickle.load(file)
     return f
+
+
+def rebuild_pickle():
+    success = True
+    # 尝试从硬盘中读取pickle
+    try:
+        data = load_pickle_from_disk(
+            r'C:\Users\ZHIYUAN\PycharmProjects\Facepro\data\pickleHere\embeddings.pickle')
+    # 捕获异常
+    # 尝试通过已有图像重新生成pickle
+    except FileNotFoundError:
+        flag = face_encoding.do_embedding()
+        if flag == 'FaceNum_Error':
+            # 程序刹车
+            success = False
+        else:
+            # 成功重新生成则再次读取pickle
+            data = load_pickle_from_disk(
+                r'C:\Users\ZHIYUAN\PycharmProjects\Facepro\data\pickleHere\embeddings.pickle')
+    return success
--- user_management.py
+++ user_management.py
@@ -1,20 +1,32 @@
 import pickle_helper
+import train_model
 import cfg_manager
 import os
 
+selected = ''  # 已选中用户名称
+directory_path = ''
+
+
 def do_management():
-    data = pickle_helper.load_pickle_from_disk(
-        r'C:\Users\ZHIYUAN\PycharmProjects\Facepro\data\pickleHere\embeddings.pickle')
-    names = data["names"]
-    name_list = []
+    # 得到name_list
+    try:
+        data = pickle_helper.load_pickle_from_disk(
+            r'C:\Users\ZHIYUAN\PycharmProjects\Facepro\data\pickleHere\embeddings.pickle')
+        names = data["names"]
+        name_list = []
+    except FileNotFoundError:
+        print("\033[1;31m[ERROR]no pickle here, please load your face and encode first\033[0m")
+        return
     for name in names:
         if name not in name_list:
             name_list.append(name)
     print('|  id  |      name      |')
     for i in range(len(name_list)):
         print(' %i       %s' % (i, name_list[i]))
+
+    # 选定操作用户
     flag = True
-    selected = ''
+    global selected
     while flag:
         try:
             n = int(input('\033[4;33menter an id :\033[0m'))
@@ -25,6 +37,7 @@ def do_management():
         except IndexError:
             print('index out of range')
     try:
+        global directory_path
         directory_path = cfg_manager.read_cfg('Common', 'face_directory')
         folder_names = os.listdir(os.path.sep.join([directory_path, selected]))
         lastchange = 0
@@ -34,8 +47,67 @@ def do_management():
                 lastchange = time
     finally:
         pass
+
     # 展示选中用户信息
     print('\033[1;32m=======================User Info========================\033[0m')
     print('- Name: %s' % selected)
     print('- Pic num: %i' % names.count(selected))
     print('- Last modified: %s' % lastchange)
+    print('\033[1;32m========================================================\033[0m')
+    print('press 1 for : Delete')
+    print('press 2 for : Backup')
+    # 选择对用户的操作
+    key = input('\033[4;33menter num and press enter : \033[0m')
+    function_choose(key)
+
+
+def function_choose(num):
+    # 功能选择
+    numbers = {
+        '1': delete,
+        '2': backup,
+    }
+    numbers.get(num, default)()
+
+
+def default():
+    # 在输入不属于字典的function_num时显示提示
+    print('\033[1;31mError:no such function\033[0m')
+    key = input('\033[4;33mpress num and press enter :\033[0m')
+    function_choose(key)
+
+
+def delete():
+    # 加载所有已存在用户信息
+    data = pickle_helper.load_pickle_from_disk(
+        r'C:\Users\ZHIYUAN\PycharmProjects\Facepro\data\pickleHere\embeddings.pickle')
+    knownNames = data["names"]  # pickle中已有的name
+    knownEmbeddings = data["embeddings"]  # pickle中以有的人脸编码
+
+    # 从embeddings.pickle中移除选中的用户信息
+    while 1:
+        try:
+            index = knownNames.index(selected)
+            del knownNames[index]
+            del knownEmbeddings[index]
+            print(index)
+        except ValueError:
+            break
+
+    data = {"embeddings": knownEmbeddings, "names": knownNames}
+    pickle_helper.write_pickle_to_disk(r"C:\Users\ZHIYUAN\PycharmProjects\Facepro\data\pickleHere\embeddings.pickle",
+                                       data)
+    delDir = os.path.sep.join([directory_path, selected])
+    # 彻底清理本地照片及目录
+    for root, dirs, files in os.walk(delDir, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+    os.removedirs(delDir)
+    # 重新训练模型
+    train_model.do_modeltrain()
+
+
+def backup():
+    pass
